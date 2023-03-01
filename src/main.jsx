34e479d341e2d3feef5333e15ccb39e88714602c
Friosn <franz.rios.nino@gmail.com>
2023-03-01 11:35:02 +0100

    supabase connection in main.jsx and eslint and prettier confings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { createClient } from "@supabase/supabase-js";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+//FIRST we need to connect with our supabase, for that we use our environment variables and connect to them with the following function and configuration:
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+);
+
+//SECONDLY we cover our App with the SessionContextProvider, adding the property for the supabaseClient of our previous connection and configuration --> supabase
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
+  </React.StrictMode>
+);
